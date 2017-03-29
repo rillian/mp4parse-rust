9061339b11966c6725e48666fca149c292c318d2
Ralph Giles <giles@mozilla.com>
2017-03-29 15:12:26 -0700

    Remove userdata pointer.
    This doesn't seem to be necessary to reproduce the issue.

--- nulltest/src/main.rs
+++ nulltest/src/main.rs
@@ -2,13 +2,12 @@
 ///
 /// Assertion fails on 1.18-nightly.
 
-type ReadFn = extern fn(*mut u8, usize, *mut std::os::raw::c_void) -> isize;
+type ReadFn = extern fn(*mut u8, usize) -> isize;
 
 #[repr(C)]
 #[derive(Clone)]
 pub struct mp4parse_io {
     pub read: ReadFn,
-    pub userdata: *mut std::os::raw::c_void,
 }
 
 // Even though mp4parse_parser is opaque to C, rusty-cheddar won't let us
@@ -19,7 +18,7 @@ struct Wrap {
 }
 
 unsafe extern fn mp4parse_new(io: *const mp4parse_io) -> *mut Wrap {
-    if io.is_null() || (*io).userdata.is_null() {
+    if io.is_null() {
         return std::ptr::null_mut();
     }
     // is_null() isn't available on a fn type because it can't be null (in
@@ -38,15 +37,13 @@ unsafe extern fn mp4parse_new(io: *const mp4parse_io) -> *mut Wrap {
 }
 
 fn boom() {
-    let mut dummy = 42;
-    unsafe {
+    let parser = unsafe {
         let io = mp4parse_io {
             read: std::mem::transmute::<*const (), ReadFn>(std::ptr::null()),
-            userdata: &mut dummy as *mut _ as *mut std::os::raw::c_void,
         };
-        let parser = mp4parse_new(&io);
-        assert!(parser.is_null());
-    }
+        mp4parse_new(&io)
+    };
+    assert!(parser.is_null());
 }
 
 fn main() {
