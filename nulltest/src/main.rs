84a60166c0813204fa81ba79ffb097976b974a5c
Ralph Giles <giles@mozilla.com>
2017-03-29 15:15:59 -0700

    Remove Wrap struct.

--- nulltest/src/main.rs
+++ nulltest/src/main.rs
@@ -10,17 +10,7 @@ pub struct mp4parse_io {
     pub read: ReadFn,
 }
 
-// Even though mp4parse_parser is opaque to C, rusty-cheddar won't let us
-// use more than one member, so we introduce *another* wrapper.
-#[repr(C)]
-struct Wrap {
-    io: mp4parse_io,
-}
-
-unsafe extern fn mp4parse_new(io: *const mp4parse_io) -> *mut Wrap {
-    if io.is_null() {
-        return std::ptr::null_mut();
-    }
+unsafe extern fn mp4parse_new(io: *const mp4parse_io) -> *mut mp4parse_io {
     // is_null() isn't available on a fn type because it can't be null (in
     // Rust) by definition.  But since this value is coming from the C API,
     // it *could* be null.  Ideally, we'd wrap it in an Option to represent
@@ -29,9 +19,7 @@ unsafe extern fn mp4parse_new(io: *const mp4parse_io) -> *mut Wrap {
     if ((*io).read as *mut std::os::raw::c_void).is_null() {
         return std::ptr::null_mut();
     }
-    let parser = Box::new(Wrap {
-        io: (*io).clone(),
-    });
+    let parser = Box::new( (*io).clone() );
 
     Box::into_raw(parser)
 }
